b41eed912dd1d13e76e2fb39367139b47e5529d0
beakal <bktotient@gmail.com>
2022-06-25 16:42:36 -0400

    add market place

---
+++ packages/hardhat/contracts/MarketPlace.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import "./IDyve.sol";
+import "./Orders.sol";
+
+contract Marketplace {
+
+
+    /**
+    * @dev short a listed NFT by it's hash
+     */
+    function sellNFT(address _nft, uint _id, address _token, uint tokenAmount) external {
+        IERC721(_nft).transferFrom(msg.sender, address(this), _id);
+        require(IERC20(_token).transferFrom(address(this), msg.sender, tokenAmount), "Unable to transfer to caller");
+    }
+}
+    
